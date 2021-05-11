996c28fcfc8138d3be57cc63943746d92a360818
donny <dongook.son@yonsei.ac.kr>
2021-05-11 14:39:55 +0900

    obsidian nord theme1

--- fish/config.fish
+++ fish/config.fish
@@ -111,6 +111,5 @@ end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /Users/dongookson/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
