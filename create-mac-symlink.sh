c44f7597543b32add37c0868b6957e00c72ff950
donny <dongook.son@yonsei.ac.kr>
2021-05-14 10:59:47 +0900

    karabiner error fixed

--- create-mac-symlink.sh
+++ create-mac-symlink.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-ln -sfn /Volumes/home/configs/coc /Users/$(whoami)/.config/coc
-ln -sfn /Volumes/home/configs/expressvpn /Users/$(whoami)/.config/expressvpn
-ln -sfn /Volumes/home/configs/rstudio /Users/$(whoami)/.config/rstudio
-ln -sfn /Volumes/home/configs/alacritty /Users/$(whoami)/.config/alacritty
-ln -sfn /Volumes/home/configs/configstore /Users/$(whoami)/.config/configstore
-ln -sfn /Volumes/home/configs/karabiner /Users/$(whoami)/.config/karabiner
-ln -sfn /Volumes/home/configs/obsidianDefault /Users/$(whoami)/.config/obsidianDefault
-ln -sfn /Volumes/home/configs/iterm2 /Users/$(whoami)/.config/iterm2
-ln -sfn /Volumes/home/configs/iterm2-profiles /Users/$(whoami)/.config/iterm2-profiles
-ln -sfn /Volumes/home/configs/aliases /Users/$(whoami)/.config/aliases
+ln -sfn /Volumes/home/configs/coc /Users/$(whoami)/.config
+ln -sfn /Volumes/home/configs/expressvpn /Users/$(whoami)/.config
+ln -sfn /Volumes/home/configs/rstudio /Users/$(whoami)/.config
+ln -sfn /Volumes/home/configs/alacritty /Users/$(whoami)/.config
+ln -sfn /Volumes/home/configs/configstore /Users/$(whoami)/.config
+ln -sfn /Volumes/home/configs/karabiner /Users/$(whoami)/.config
+ln -sfn /Volumes/home/configs/obsidianDefault /Users/$(whoami)/.config
+ln -sfn /Volumes/home/configs/iterm2 /Users/$(whoami)/.config
+ln -sfn /Volumes/home/configs/iterm2-profiles /Users/$(whoami)/.config
+ln -sfn /Volumes/home/configs/aliases /Users/$(whoami)/.config
 ln -sfn /Volumes/home/configs/aliases /Users/$(whoami)/.config/fish/aliases.fish
+# if karabiner does not work, go to Sys Pref > Security and allow
+# karabiner elements to have full disk access
