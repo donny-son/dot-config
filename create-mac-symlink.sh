9d03355cecae583afb6a1ad0d09036fa86e68bb4
donny <dongook.son@yonsei.ac.kr>
2021-05-14 09:58:33 +0900

    symlink script

---
+++ create-mac-symlink.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+ln -sfn /Volumes/home/configs/coc /Users/$(whoami)/.config/coc
+ln -sfn /Volumes/home/configs/expressvpn /Users/$(whoami)/.config/expressvpn
+ln -sfn /Volumes/home/configs/rstudio /Users/$(whoami)/.config/rstudio
+ln -sfn /Volumes/home/configs/htop /Users/$(whoami)/.config/htop
+ln -sfn /Volumes/home/configs/alacritty /Users/$(whoami)/.config/alacritty
+ln -sfn /Volumes/home/configs/configstore /Users/$(whoami)/.config/configstore
+ln -sfn /Volumes/home/configs/karabiner /Users/$(whoami)/.config/karabiner
+ln -sfn /Volumes/home/configs/obsidianDefault /Users/$(whoami)/.config/obsidianDefault
+ln -sfn /Volumes/home/configs/iterm2 /Users/$(whoami)/.config/iterm2
+ln -sfn /Volumes/home/configs/iterm2-profiles /Users/$(whoami)/.config/iterm2-profiles
+ln -sfn /Volumes/home/configs/aliases /Users/$(whoami)/.config/aliases
+ln -sfn /Volumes/home/configs/aliases /Users/$(whoami)/.config/fish/aliases.fish
+ln -sfn /Volumes/home/configs/neofetch /Users/$(whoami)/.config/neofetch
+ln -sfn /Volumes/home/configs/fish/fish_variables /Users/$(whoami)/.config/fish/fish_variables
