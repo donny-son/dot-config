5f8f5207054a2443ce75f04bd7df4c804329e546
donny <dongook.son@yonsei.ac.kr>
2021-09-09 11:48:08 +0900

    add logo

--- fish/config.fish
+++ fish/config.fish
@@ -112,6 +112,6 @@ end
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+eval /Users/dongookson/opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
