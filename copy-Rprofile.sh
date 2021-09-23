6933d7f7e384447dd154e8e92328819cbb190f63
donny <dongook.son@yonsei.ac.kr>
2021-09-23 09:41:27 +0900

    rprofile script

---
+++ copy-Rprofile.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+dst=$HOME/.Rprofile
+echo "Adding default configurations to ${dst}"
+
+cat >> $dst <<EOF
+# set default repository to cran
+local({r <- getOption("repos")
+       r["CRAN"] <- "http://cran.r-project.org"
+       options(repos=r)})
+
+# set default q function to quit without saving workspace
+utils::assignInNamespace(
+  "q", 
+  function(save = "no", status = 0, runLast = TRUE) 
+  {
+    .Internal(quit(save, status, runLast))
+  }, 
+  "base"
+)
+EOF
+
